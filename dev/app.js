9482eef20197afdb714cad0af489e3fa81df242e
Ricardo <landinezrichard@gmail.com>
2016-01-26 20:02:11 -0500

    Añadido menu, y funcion para animar al hacer click en el menu

--- dev/app.js
+++ dev/app.js
@@ -1,18 +1,24 @@
 (function () {
 	/* Dependencias*/
 	const $ = require('jquery');
-	// const showMenu = require('./components/MainMenu');
+	const showMenu = require('./components/MainMenu');
 	const ScrollMagic = require('scrollmagic');
+	const gsap = require('gsap');
 	require('scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators');
+	require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
+	require('./lib/ScrollToPlugin.min.js');
 	
 	document.addEventListener('DOMContentLoaded', onDOMload);
 	
-	function onDOMload() {		
+	function onDOMload() {
+
+		showMenu.init();	
 		
 		// Init ScrollMagic
 		var ctrl = new ScrollMagic.Controller({
 			globalSceneOptions: {
-				triggerHook: 'onLeave'//definimos cuando se dispara la animación al hacer scroll
+				triggerHook: 'onLeave',//definimos cuando se dispara la animación al hacer scroll
+				reverse: true
 			}
 		});
 
@@ -44,6 +50,39 @@
 		.setClassToggle("section#four", "is-active")
 		.addIndicators()
 		.addTo(ctrl);
+
+		ctrl.scrollTo(function(target) {
+
+			TweenMax.to(window, 0.5, {
+				scrollTo : {
+				y : target, // scroll position of the target along y axis
+				autoKill : true // allows user to kill scroll action smoothly
+				},
+				ease : Cubic.easeInOut
+			});
+
+		});
+
+
+		//  Bind scroll to anchor links
+		$('#menu').on("click", "a", function(e) {
+			showMenu.close();
+
+			var id = $(this).attr("href");
+
+			if($(id).length > 0) {
+				e.preventDefault();
+
+				// trigger scroll
+				ctrl.scrollTo(id);
+
+				// If supported by the browser we can also update the URL
+				if (window.history && window.history.pushState) {
+					history.pushState("", document.title, id);
+				}
+			}
+
+		});
 		
 	}
 
